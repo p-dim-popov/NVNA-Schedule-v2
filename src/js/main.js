98d4198073e682d0d3d458ce288ebdabad473daf
pdimp <p.dim.popov@gmail.com>
2020-09-08 23:35:35 +0300

    remove warmup step in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,6 @@
 import {registerServiceWorker, installPrompt} from "./install"
 import run from "./run";
 
-const webScrapper = `https://web--scrapper.herokuapp.com/webscrapper`;
-//"warm up" step
-if (process.env.NODE_ENV === "production")
-    fetch(`${webScrapper}?url=${encodeURIComponent("http://schedule.nvna.free.bg")}`)
-
 run();
 registerServiceWorker();
 installPrompt();
