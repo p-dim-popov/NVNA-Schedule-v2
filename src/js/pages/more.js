78118a9230205fe6915b13c9d7bee682b585a09f
pdimp <p.dim.popov@gmail.com>
2020-10-07 15:07:54 +0300

    quick fix lecturer codes update

--- src/js/pages/more.js
+++ src/js/pages/more.js
@@ -4,8 +4,5 @@ export async function showTimetable() {
 
 export async function showLecturerCodes() {
     this.content.innerHTML +=
-    `<iframe id="lecturer-codes" style="position:absolute;border:none;width:100%;height:100%"/>`
-    this.content.querySelector("#lecturer-codes").outerHTML =
-        (await fetch(`https://web-harvester.herokuapp.com/?url=${encodeURIComponent("https://nvna.eu/wp/wp-content/uploads/2015/12/kodove_prepodavateli_2020_2021.pdf")}`)
-            .then(r => r.json())).contents;
+        `<iframe src="https://nvna.eu/wp/wp-content/uploads/2015/12/kodove_prepodavateli_2020_2021.pdf" id="lecturer-codes" style="position:absolute;border:none;width:100%;height:100%"/>`
 }
