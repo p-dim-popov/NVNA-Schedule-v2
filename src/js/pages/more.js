3e194b186ebaed42cd0d043ee8055763fd723d97
pdimp <p.dim.popov@gmail.com>
2020-10-07 15:05:26 +0300

    update lecturer codes link

--- src/js/pages/more.js
+++ src/js/pages/more.js
@@ -6,6 +6,6 @@ export async function showLecturerCodes() {
     this.content.innerHTML +=
     `<iframe id="lecturer-codes" style="position:absolute;border:none;width:100%;height:100%"/>`
     this.content.querySelector("#lecturer-codes").outerHTML =
-        (await fetch(`https://web-harvester.herokuapp.com/?url=${encodeURIComponent("http://old-www.naval-acad.bg/infocenter/razpisanij/novo/kod-prepodav.htm")}`)
+        (await fetch(`https://web-harvester.herokuapp.com/?url=${encodeURIComponent("https://nvna.eu/wp/wp-content/uploads/2015/12/kodove_prepodavateli_2020_2021.pdf")}`)
             .then(r => r.json())).contents;
 }
